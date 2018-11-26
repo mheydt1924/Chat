1bba27bbbed89dcd08afb70a50700a640893af68
maeda <hty2454115aaa@gmail.com>
2018-11-26 15:47:25 +0900

    delete write.php

--- get.php
+++ get.php
@@ -1,5 +1,37 @@
 <?php
+require('lib.php');
+
 $result = [];
+
+//-------------------------------------------------
+//準備
+//-------------------------------------------------
+// 実行したいSQL
+$sql = 'SELECT * FROM log';
+
+//-------------------------------------------------
+//SQLを実行
+//-------------------------------------------------
+$dbh = connectDB();						//接続
+$sth = $dbh->prepare($sql);         	//SQL準備
+$sth->execute();                    	//実行
+
+//取得した内容を表示する
+while(true){
+    //ここで1レコード取得
+    $buff = $sth->fetch(PDO::FETCH_ASSOC);
+    if( $buff === false ){
+        break;    //データがもう存在しない場合はループを抜ける
+    }
+    
+    $result[] = [
+    	  "name"    => $buff["name"]
+		, "message" => $buff["message"]
+		, "time"    => $buff["time"]
+	];
+};
+
+/*
 $fp = fopen("data.txt", "r");
 while( ($buff=fgets($fp)) != false ){
 	$line = explode("\t", $buff);
@@ -10,4 +42,6 @@ while( ($buff=fgets($fp)) != false ){
 	];
 }
 fclose($fp);
+*/
+
 echo json_encode($result);
